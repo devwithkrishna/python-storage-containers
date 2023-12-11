964f8dbe1b09122d721c1aeeaab9affd55b4fc51
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2023-12-11 22:12:03 +0530

    DEVOPS-16 added more functionalities

--- storage_blob.py
+++ storage_blob.py
@@ -6,7 +6,7 @@ from get_secret_from_kv import get_access_key_from_kv
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, generate_account_sas, ResourceTypes, AccountSasPermissions
-
+from azure.storage.blob import ContainerClient
 
 def get_storage_account_url(storage_account: str):
     """
@@ -44,10 +44,46 @@ def delete_container_on_storage_account(storage_account:str, container_name: str
         container_name:
     Returns:
     """
+    container_client = BlobServiceClient.undelete_container()
     blob_service_client = create_blob_service_client(storage_account=storage_account)
     blob_service_client.delete_container(container=container_name)
     print(f"Container {container_name} deleted from {storage_account}")
 
+
+def recover_soft_deleted_containers_on_storage_account(storage_account: str, container_name: str):
+    """
+    recover soft deleted containers.
+    Operation will only be successful if used within the specified number of days set in the delete retention policy.
+
+    Args:
+        storage_account:
+        container_name:
+
+    Returns:
+
+    """
+    blob_service_client = create_blob_service_client(storage_account=storage_account)
+    recovered_containers = blob_service_client.undelete_container(container_name, deleted_container_version = '')
+    print(f"Container {container_name} recovered from {storage_account}")
+
+
+def list_containers_on_storage_account(storage_account: str):
+    """
+    function to return name of containers in mentioned storage account
+    Args:
+        storage_account:
+
+    Returns:
+    list of containers
+    """
+    blob_service_client = create_blob_service_client(storage_account=storage_account)
+    list_of_containers = blob_service_client.list_containers()
+    for container in list_of_containers:
+        containername = container.name
+        container_lastmodifiedd = container.last_modified
+        print(f"Container name is : {containername}")
+        print(f"Container last modified on : {container_lastmodifiedd}")
+    print(f"Containers listed on {storage_account}")
 
 def create_blob_service_client(storage_account: str):
     """
@@ -92,7 +128,7 @@ def azure_storage_blob(storage_account: str, time_limit_of_sas_token: int, keyva
     )
 
     print(f"SAS Token for {storage_account} for {time_limit_of_sas_token} hour is: {sas_token}")
-    storage_account_url = get_storage_account_url(storage_account=storage_account)
+    # storage_account_url = get_storage_account_url(storage_account=storage_account)
     
     if action == "create_container":
         create_container_on_storage_account(storage_account=storage_account, container_name=container_name)
@@ -100,6 +136,12 @@ def azure_storage_blob(storage_account: str, time_limit_of_sas_token: int, keyva
     elif action == "delete_container":
         delete_container_on_storage_account(storage_account=storage_account,container_name=container_name)
         print(f"Action {action} {container_name} on {storage_account}")
+    elif action == "list_containers":
+        list_containers_on_storage_account(storage_account=storage_account)
+        print(f"Action {action} on storage account {storage_account}")
+    elif action == "recover_container":
+        recover_soft_deleted_containers_on_storage_account(storage_account=storage_account,container_name=container_name)
+        print(f"Action {action} on storage account {storage_account}")
     else:
         print(f"provided invalid action. please provide one among the available choices")
 
@@ -111,10 +153,10 @@ def main():
     """
     parser = argparse.ArgumentParser (description='argumets for the function')
     parser.add_argument('--storage_account', help= 'storage account name', type= str, nargs='?', required=True)
-    parser.add_argument('--time_limit_of_sas_token', help='time limit of SAS token', type=int, default= 1, required=True)
+    parser.add_argument('--time_limit_of_sas_token', help='time limit of SAS token', type=float, default= 1, required=True)
     parser.add_argument('--keyvault_name', help='keyvault name in which storage account key secret set', type=str)
     parser.add_argument('--secret_name', help='storage account access key secret name', type=str)
-    parser.add_argument('--action', required=True, help= 'create container / delete container/ etc', type= str, choices=['create_container', 'delete_container',], type=str)
+    parser.add_argument('--action', required=True, help= 'create container / delete container/ etc', type= str, choices=['create_container', 'delete_container','list_containers','recover_container'])
     parser.add_argument('--container_name', type=str, help='container name')
     args = parser.parse_args()
     storage_account = args.storage_account
